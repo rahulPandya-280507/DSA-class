76734f8bdb3e61da7ffbc21c632bf2e71e5e5232
rahulPandya-280507 <rahulpandya280507@gmail.com>
2026-02-17 08:06:50 +0530

    some random BS

---
+++ bubblesort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int temp,n;
+    int arr[50];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+        {
+            scanf("%d",&arr[i]);
+        }
+  
+    for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+                {
+                    if (arr[j]>arr[j+1])
+                        {
+                            temp=arr[j];
+                            arr[j]=arr[j+1];
+                            arr[j+1]=temp;
+                        }
+                        
+                }
+                
+        }
+    for(int i=0;i<n;i++)
+        {
+            printf("\n%d",arr[i]);
+        }
+    return 0;
+}
